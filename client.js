8a1833b5e3f9fe8e337612b39f303e25d0489e33
Ersel Er <ersel99@gmail.com>
2016-02-19 03:50:09 +0200

    semantic-ui framework used and login page customized

--- client.js
+++ client.js
@@ -9,7 +9,8 @@ var user = client.record.getRecord('user');
 $(document).ready(function(){
     $('#chat-room').hide();
     $('#private-chat').hide();
-    $('#login-button').on('click', function(){
+    $('#login-form').submit(function(e){
+        e.preventDefault();
         console.log("button pressed!")
 
         var name = $('#name').val();
@@ -50,7 +51,6 @@ $(document).ready(function(){
     //////////                   CHAT
     ///////////////////////////////////////////////////////////////////
     client.event.subscribe('chat', function(data){
-        var CSS_COLOR_NAMES = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"];
         var user = users.get()[data.uid]
         var li = $('<li>');
         li.append('<span style="color:' + user.color + ';">' + user.firstname + ' ' + user.lastname + ': </span>' + data.msg)
@@ -62,8 +62,10 @@ $(document).ready(function(){
         //console.log(user.firstname + " " + user.lastname + ": " + data.msg)
     })    
 
-    $('#send-msg').on('click', function(){
+    $('#post-msg-form').submit(function(e){
+        e.preventDefault();
         var msg = $('#msg-content').val()
+        $('#msg-content').val('');
         client.event.emit( 'chat', {'msg':msg, 'uid':uid});
     })
     //////////////////////////////////////////////////////////////////
