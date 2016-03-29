45d11890b5f34c5576b097c104ac4dc30f8e380e
Ersel Er <ersel99@gmail.com>
2016-03-29 03:33:42 +0300

    package.json added

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var DeepstreamServer = require( 'deepstream.io' ),
 
 // Optionally you can specify some settings, a full list of which
 // can be found here http://deepstream.io/docs/deepstream.html
-server.set( 'host', 'localhost' );
+server.set( 'host', '0.0.0.0' );
 server.set( 'port', 6020 );
 
 // start the server
